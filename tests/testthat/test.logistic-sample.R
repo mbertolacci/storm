6693a201f59287698dbba38c6168044e9f96f30d
Michael Bertolacci <m.bertolacci@gmail.com>
2016-05-31 13:44:07 +0800

    Add non-panel test for logistic-sample

--- tests/testthat/test.logistic-sample.R
+++ tests/testthat/test.logistic-sample.R
@@ -1,24 +1,38 @@
 context('ptsm_logistic_sample')
 
-# test_that('ptsm_logistic_generate generates non-panel data', {
-#     input_data <- data.frame(t=seq(0, 1, length.out=100))
+test_that('ptsm_logistic_sample on non-panel data', {
+    input_data <- data.frame(t=seq(0, 1, length.out=100))
 
-#     formula <- ~ t + I(t ^ 2)
-#     distributions <- c('gamma', 'gamma')
-#     component_parameters <- list(c(1.5, 1), c(1.18, 10))
-#     delta <- rbind(
-#         c(1, 1, 1, 1, 1),
-#         c(-1, -1, -1, -1, -1)
-#     )
+    formula <- ~ t + I(t ^ 2)
+    distributions <- c('gamma', 'gamma')
+    component_parameters <- list(c(1.5, 1), c(1.18, 10))
+    n_deltas <- 5
+    delta <- rbind(
+        c(1, 1, 1, 1, 1),
+        c(-1, -1, -1, -1, -1)
+    )
 
-#     output <- ptsm_logistic_generate(
-#         input_data, formula, distributions, component_parameters,
-#         delta=delta
-#     )
+    generated <- ptsm_logistic_generate(
+        input_data, formula, distributions, component_parameters,
+        delta=delta
+    )
+    data <- input_data
+    data$y <- generated$data$y
 
-#     expect_equal(nrow(output$data), 100)
-#     expect_equal(output$delta, delta)
-# })
+    n_samples <- 20
+
+    output <- ptsm_logistic_sample(
+        n_samples=n_samples, burn_in=5,
+        data, y ~ t + I(t ^ 2), distributions,
+        theta_sample_thinning=1, z_sample_thinning=1, y_missing_sample_thinning=0,
+        verbose=0
+    )
+
+    expect_equal(nrow(output$theta_sample), n_samples)
+    expect_equal(ncol(output$theta_sample), 4 + 2 * n_deltas)
+    expect_equal(nrow(output$z_sample), n_samples)
+    expect_equal(ncol(output$z_sample), 100)
+})
 
 get_sample <- function(n_levels, n_per_level, n_samples) {
     input_data <- data.frame(
