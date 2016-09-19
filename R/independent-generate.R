8b2c0164e7069bec5d0e2834461c8e6af2678583
Michael Bertolacci <m.bertolacci@gmail.com>
2016-09-19 23:50:37 +1000

    Allow ptsm_independent_generate to use more than 3 components

--- R/independent-generate.R
+++ R/independent-generate.R
@@ -1,6 +1,6 @@
 #' @export
 ptsm_independent_generate <- function(n, p, distributions, component_parameters) {
-    z <- sample.int(3, n, replace = TRUE, prob = p)
+    z <- sample.int(length(distributions) + 1, n, replace = TRUE, prob = p)
     y <- .y_given_z(z, distributions, component_parameters)
 
     return(data.frame(z, y))
