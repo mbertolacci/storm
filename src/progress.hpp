3a4144d72a5f03a9710261921b03538a16ba83ae
Michael Bertolacci <m.bertolacci@gmail.com>
2018-05-02 11:34:27 +0800

    Make progress bar look nice in R terminal

--- src/progress.hpp
+++ src/progress.hpp
@@ -11,7 +11,8 @@ class ProgressBar {
         : nSteps_(nSteps),
           currentStep_(0),
           lastCheckStep_(0),
-          lastCheckStepTime_(0) {
+          lastCheckStepTime_(0),
+          lastOutputLength_(0) {
         lastCheckTime_ = std::chrono::system_clock::now();
     }
 
@@ -34,11 +35,12 @@ class ProgressBar {
     uint64_t lastCheckStep_;
     std::chrono::time_point<std::chrono::system_clock> lastCheckTime_;
     double lastCheckStepTime_;
+    unsigned int lastOutputLength_;
 
     void output() {
         unsigned int percent = 100 * currentStep_ / nSteps_;
 
-        if (currentStep_ == 1 || currentStep_ % 10 == 0) {
+        if (currentStep_ == 1 || currentStep_ % 100 == 0) {
             std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
             std::chrono::duration<double> elapsedSeconds = now - lastCheckTime_;
             lastCheckStepTime_ = 1000 * elapsedSeconds.count() / (currentStep_ - lastCheckStep_);
@@ -47,10 +49,22 @@ class ProgressBar {
             lastCheckTime_ = now;
         }
 
-        Rcpp::Rcout << "\r\033[2K"
-          << currentStep_ << "/" << nSteps_ << " (" << percent << "%)"
-          << " " << lastCheckStepTime_ << "ms/iteration"
-          << " (" << ((nSteps_ - currentStep_) * lastCheckStepTime_) / 1000 << "s remaining)";
+        // Write out a string of spaces to overwrite the last output, because the
+        // internal R terminal doesn't support ANSI codes
+        Rcpp::Rcout << "\r";
+        for (unsigned int i = 0; i < lastOutputLength_; ++i) {
+            Rcpp::Rcout << " ";
+        }
+
+        std::ostringstream sstream;
+        sstream << std::fixed << std::setprecision(2)
+            << "\r"
+            << currentStep_ << "/" << nSteps_ << " (" << percent << "%)"
+            << " " << lastCheckStepTime_ << "ms/iteration"
+            << " (" << ((nSteps_ - currentStep_) * lastCheckStepTime_) / 1000 << "s remaining)";
+        std::string output = sstream.str();
+        lastOutputLength_ = output.length();
+        Rcpp::Rcout << output;
 
         if (currentStep_ == nSteps_) {
             Rcpp::Rcout << "\n";
